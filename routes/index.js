91d9e2d454c00c7569e9109a695e9bf0517e050d
GeStanley <geoffrey.stanley@gmail.com>
2015-07-23 22:19:10 -0400

    got basicl mdl working on website

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/list', function(req, res, next) {
+  res.render('list');
+});
+
+router.get('/html', function(req, res, next) {
+  res.render('testhtml.html');
+});
+
 module.exports = router;
